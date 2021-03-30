afbb61c60db821e51b34ea6ac469dafc3994470a
Liarperez <Lisandro@MacPro.local>
2021-03-30 10:44:19 -0400

    Modified LAP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello LisandroArbellaPerez!"));
+app.get("/", (req, res) => res.send("Hello LAP"));
 
 app.get("/health", (req, res) => {
   res.status(200);
